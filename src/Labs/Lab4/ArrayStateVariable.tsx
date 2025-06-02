fa64c85be83149a7c936b04c5fd839adafaed434
zdnniz <zdennis076@gmail.com>
2025-06-02 11:36:02 -0400

    a4 Redux

--- src/Labs/Lab4/ArrayStateVariable.tsx
+++ src/Labs/Lab4/ArrayStateVariable.tsx
@@ -1,11 +1,11 @@
-import React, { useState } from "react";
+import { useState } from "react";
 export default function ArrayStateVariable() {
     const [array, setArray] = useState([1, 2, 3, 4, 5]);
     const addElement = () => {
         setArray([...array, Math.floor(Math.random() * 100)]);
     };
     const deleteElement = (index: number) => {
-        setArray(array.filter((item, i) => i !== index));
+        setArray(array.filter((i) => i !== index));
     };
     return (
         <div id="wd-array-state-variables">
